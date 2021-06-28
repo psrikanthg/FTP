75900f1f979c97ac16a00b951ff4f54a4c652008
Srikanth P <psrikanthg.0303@gmail.com>
2021-06-28 16:31:53 +0800

    Product added

---
+++ src/com/giga/timetac/ftp/Product.java
@@ -0,0 +1,43 @@
+package com.giga.timetac.ftp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+	int id;
+	String name;
+	float price;
+	
+	public Product(int id, String name, float price) {
+		
+		this.id = id;
+		this.name =name;
+		this.price = price;
+	}
+
+	
+	public static void main(String args[]) {
+		
+		List<Product> productList= new ArrayList<>();
+		
+		
+		
+		productList.add(new Product(1,"HP Laptop", 25000f));
+		productList.add(new Product(1,"Dell Laptop", 30000f));
+		productList.add(new Product(1,"Lenevo Laptop", 28000f));
+		productList.add(new Product(1,"Soney Laptop", 28000f));
+		productList.add(new Product(1,"Apple Laptop", 90000f));
+		List<Float> productPriceList = new ArrayList<>();
+		
+		for(Product product: productList) {
+			
+			if(product.price <30000 ) {
+				productPriceList.add(product.price);
+			}
+		}
+		
+		System.out.println(productPriceList);
+		
+	}
+	
+}
